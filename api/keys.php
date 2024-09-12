7e6ef97f03012a34178cc0987b09dc9cb76ada48
Babel-In <babel.india.2024@gmail.com>
2024-09-12 11:55:35 +0400

    V2

--- api/keys.php
+++ api/keys.php
@@ -6,10 +6,7 @@ header("Cache-Control: max-age=20, public");
 $id = $_GET['id'] ?? exit;
 $api = "https://cdn.babel-in.xyz/tpck/index.php?id=$id";
 $userAgent = 'Babel-IN'; // u can change if u wanna
-
 $json = fetchMPDManifest($api, $userAgent);
-$data = json_decode($json, true);
-$keys = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
 
 header('Content-Type: application/json');
-echo $keys;
+echo $json;
